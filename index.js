0663435144ef392dfc1bc3713359819429762676
timothynicolas <142022974+timothynicolas@users.noreply.github.com>
2025-01-04 14:52:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const currentPage = window.location.pathname;
+
+    if (currentPage === '/index.html') {
+        alert("Hello! Welcome to my Website! - NICOLAS, Paul Timothy L.")
+    } 
+});
+
+console.log(window.location.pathname);
+
 const aboutButton = document.getElementById("about-under-dev");
 const contactButton = document.getElementById("contact-under-dev");
 const mainContent = document.getElementById('main-content');
 
+
+
 const popup = document.getElementById('popup');
 const closePopup = document.getElementById('close-popup');
+
 
 function showPopup() {
     popup.style.display = 'flex'; 
@@ -11,11 +24,13 @@ function showPopup() {
     footer.classList.add('blur');
 }
 
+
 function hidePopup() {
     popup.style.display = 'none';
     mainContent.classList.remove('blur');
     footer.classList.remove('blur');
 }
+
 
 aboutButton.addEventListener('click', showPopup);
 contactButton.addEventListener('click', showPopup);
